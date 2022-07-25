c7aa340518a194740cae3bfe36f293b8f937b102
Dixel9 <diogononatoux@gmail.com>
2022-07-25 13:00:04 -0300

    intent nativa e bug fix

--- app/src/main/java/me/dio/soccernews/ui/adapters/NewsAdapter.java
+++ app/src/main/java/me/dio/soccernews/ui/adapters/NewsAdapter.java
@@ -37,20 +37,20 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         News news = this.news.get(position);
-        holder.binding.tvTitle.setText(news.getTitle());
-        holder.binding.tvDescription.setText(news.getDescription());
-        Picasso.get().load(news.getImage()).fit().into(holder.binding.ivThumbnail);
+        holder.binding.tvTitle.setText(news.title);
+        holder.binding.tvDescription.setText(news.description);
+        Picasso.get().load(news.image).fit().into(holder.binding.ivThumbnail);
         holder.binding.btOpenLink.setOnClickListener(view ->{
             Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(news.getLink()));
+            i.setData(Uri.parse(news.link));
             holder.itemView.getContext().startActivity(i);
         });
         //implementação da funcionalidade de compartilhar.
         holder.binding.ivShare.setOnClickListener(view ->{
             Intent i = new Intent(Intent.ACTION_SEND);
             i.setType("text/plain");
-            i .putExtra(android.content.Intent.EXTRA_SUBJECT, news.getTitle());
-            i.putExtra(Intent.EXTRA_TEXT,news.getLink());
+            i .putExtra(android.content.Intent.EXTRA_SUBJECT, news.title);
+            i.putExtra(Intent.EXTRA_TEXT,news.link);
             holder.itemView.getContext().startActivity(Intent.createChooser(i, "Share via"));
         });
         //implementação da funcionalidade de compartilhar. (o evento será instanciado pelo fragmento)
